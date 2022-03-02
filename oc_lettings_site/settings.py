797e2edfd47648b39bca8b554e37726160e980d6
= <madgic.shrooms@gmail.com>
2022-03-02 16:02:02 +0100

    Test deployment heroku

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['oc-p13-lettings.herokuapp', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
