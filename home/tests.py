765ad9a7cc7c865a33c2f65286a73cad38ef40a3
= <madgic.shrooms@gmail.com>
2022-03-08 19:17:01 +0100

    test ok

--- home/tests.py
+++ home/tests.py
@@ -10,7 +10,7 @@ class TestHomeApp(TestCase):
         path = reverse('home')
         response = self.client.get(path)
         assert response.status_code == 200
-        assertTemplateUsed(response, 'home/indexx.html')
+        assertTemplateUsed(response, 'home/index.html')
 
     def test_home_url(self):
         path = reverse('home')
