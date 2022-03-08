ace8a4bc044b1a029f573082b8bf34dcd5f1049d
= <madgic.shrooms@gmail.com>
2022-03-08 19:51:12 +0100

    Add Sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -113,3 +113,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+sentry_sdk.init(
+    dsn="https://4f0f627387b0428dbd79b4c8221b4e85@o1146393.ingest.sentry.io/6249421",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
